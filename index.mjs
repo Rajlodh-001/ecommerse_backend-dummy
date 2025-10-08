d3d9c177065c5d28fb7c8e8e329204ac01c65a59
Rajlodh-001 <Rajlodh12345@gmail.com>
2025-10-08 19:47:43 +0530

    seperating app file for further testing

--- index.mjs
+++ index.mjs
@@ -1,20 +1,10 @@
-import express, { json } from "express";
-import productRoutes from "./src/routes/productRoutes.mjs"
-import optionRoutes from "./src/routes/optionRoutes.mjs";
-import cartRoutes from "./src/routes/cartRoutes.mjs";
-import addressRoutes from "./src/routes/addressRoutes.mjs";
-const app=express();
 
-const PORT =3000;
+import app from "./app.mjs"
 
-app.use(json())
-app.use(productRoutes);
-app.use(optionRoutes);
-app.use(cartRoutes);
-app.use(addressRoutes);
-app.get("/",(req,res)=>{
-    res.send("Test API call for E-commerse Dummy ")
-})
+
+const PORT = process.env.PORT || 3000;
+
+
 
 
 app.listen(PORT, () => {
